cd32fc6dc7898c38450f6e57f3c714332ee6350e
Leona Yang <leona.y.young@gmail.com>
2021-03-06 05:23:36 +0800

    add react-router-dom

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import 'antd/dist/antd.css';
 import { Layout, Menu } from 'antd';
 import {
@@ -9,6 +10,7 @@ import {
   UploadOutlined,
 } from '@ant-design/icons';
 import './style.css';
+import HomePage from '../homePage/index';
 import UserManage from '../userManage/index.js';
 
 const { Header, Sider, Content } = Layout;
@@ -22,38 +24,47 @@ function Index() {
 
   return (
     <Layout id="components-layout-demo-custom-trigger">
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo">React Awesome</div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }}>
-          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-            className: 'trigger',
-            onClick: toggle,
-          })}
-        </Header>
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          <UserManage />
-        </Content>
-      </Layout>
+      <Router>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo">React Awesome</div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/">HomePage</Link>
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              <Link to="/usermanage">UserManage</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/usermanage">
+                <UserManage />
+              </Route>
+            </Switch>
+          </Content>
+        </Layout>
+      </Router>
     </Layout>
   )
 }
--- src/homePage/index.js
+++ src/homePage/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { DatePicker } from 'antd';
 
 function Index() {
   return (
     <div>
       <h1>Hello React</h1>
-      <DatePicker />
     </div>
   )
 }
